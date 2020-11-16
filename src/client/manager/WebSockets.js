1fea6dfb74ee6b37f00194727ca0a6cf4d8d1fb5
afarqu <ajf@akana.com>
2020-11-16 13:39:36 -0800

    Upgrade socket.io client

--- src/client/manager/WebSockets.js
+++ src/client/manager/WebSockets.js
@@ -11,6 +11,7 @@
 
 import EventEmitter from '../../internal/EventEmitter'
 import io from "socket.io-client";
+import jwtDecode from 'jwt-decode'
 
 export default class WebSockets extends EventEmitter {
   constructor (store, client, address) {
@@ -65,9 +66,10 @@ export default class WebSockets extends EventEmitter {
     // Create the websocket
     // const url = `${this.address}/ws?app_id=${encodeURIComponent(this.store.appID)}&token=${encodeURIComponent(this.store.token)}`
     // this.socket = new WebSocket(url)
+    const decoded = jwtDecode(this.store.token);
     this.socket = io(this.address, {
 			query: {
-        eventUri: `/u/${this.store.userID}`,
+        eventUri: `/u/${decoded.sub}`,
         app_id: this.store.appID,
         token: this.store.token
 			},
